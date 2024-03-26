cdb70209497ba2f00139456881ed8cf311a385ed
fkiscd <fkiscd@gmail.com>
2024-03-26 16:55:58 +0800

    Update sensor.py

--- custom_components /sensor.py
+++ custom_components /sensor.py
@@ -1,1 +1,111 @@
+"""Platform for sensor integration."""
+from __future__ import annotations
+
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorEntityDescription,
+    SensorStateClass,
+)
+from homeassistant.const import (
+    EntityCategory,
+    UnitOfPressure,
+    UnitOfTemperature,
+    PERCENTAGE,
+    DEGREE,
+    UnitOfSpeed,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+SENSORS: tuple[SensorEntityDescription, ...] = (
+    SensorEntityDescription(
+        key="rbar",
+        name="Air Pressure",
+        device_class=SensorDeviceClass.PRESSURE,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=UnitOfPressure.HPA,
+    ),
+    SensorEntityDescription(
+        key="intem",
+        name="Indoor Temperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+    ),
+    SensorEntityDescription(
+        key="inhum",
+        name="Indoor Humidity",
+        device_class=SensorDeviceClass.HUMIDITY,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=PERCENTAGE,
+    ),
+    SensorEntityDescription(
+        key="t1tem",
+        name="Outdoor Temperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=UnitOfTemperature.CELSIUS,
+    ),
+    SensorEntityDescription(
+        key="t1hum",
+        name="Outdoor Humidity",
+        device_class=SensorDeviceClass.HUMIDITY,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=PERCENTAGE,
+    ),
+    SensorEntityDescription(
+        key="t1wdir",
+        name="Outdoor Wind Direction",
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=DEGREE,
+    ),
+    SensorEntityDescription(
+        key="t1ws",
+        name="Outdoor Wind Speed",
+        device_class=SensorDeviceClass.WIND_SPEED,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement=UnitOfSpeed.METERS_PER_SECOND,
+    ),
+)
+
+
+async def async_setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    async_add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
+    """Set up the sensor platform."""
+    async_add_entities(
+        CclSensorEntity(description)
+        for description in SENSORS
+    )
+
+
+
+class CclSensorEntity(SensorEntity):
+    """Representation of a Sensor."""
+    entity_description: SensorEntityDescription
+    _attr_entity_category = (
+        EntityCategory.DIAGNOSTIC
+    )
+    
+    def __init__(
+        self, entity_description: SensorEntityDescription
+    ) -> None:
+        self.entity_description = entity_description
+        self._attr_available = False
+        self._device_info = {}
+
+    def update(self) -> None:
+        self._attr_available = True
+
+        if (self._attr_native_value == "unknown") | (self._attr_native_value == None):
+            self._attr_native_value = 25
 
